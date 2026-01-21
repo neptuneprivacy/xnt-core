47ee12864da6b091c61eb3be3b3162b346677130
Privacyrights <gary@garygensler.org>
2026-01-21 11:13:20 -0700

    Security hotfix and Triton VM upgrade

--- scripts/linux/install-bash-completions.sh
+++ scripts/linux/install-bash-completions.sh
@@ -1,10 +1,10 @@
 set -e
 
-SCRIPT="$HOME/.bash_neptune_cli"
+SCRIPT="$HOME/.bash_xnt_cli"
 LINE="source $SCRIPT"
 FILE="$HOME/.bashrc"
 
-cargo run --bin neptune-cli -- completions > $SCRIPT
+cargo run --bin xnt-cli -- completions > $SCRIPT
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 source $SCRIPT
 echo "completions installed to $SCRIPT and added to $FILE."
