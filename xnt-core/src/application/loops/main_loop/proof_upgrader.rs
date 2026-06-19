27ffc46fbcbf0d7a78cd1f95a9c17ed041902c41
Thorkil Vaerge <thor@neptune.cash>
2026-06-19 16:22:14 +0200

    fix: Return job with highest upgrade incentive
    Return the upgrade job with the highest incentive to upgrade, not the
    one with the lowest, as was previously the case.

--- xnt-core/src/application/loops/main_loop/proof_upgrader.rs
+++ xnt-core/src/application/loops/main_loop/proof_upgrader.rs
@@ -959,7 +959,7 @@ pub(super) async fn get_upgrade_task_from_mempool(
         .collect_vec();
     jobs.sort_by_key(|job| job.upgrade_incentive());
 
-    jobs.first().cloned()
+    jobs.last().cloned()
 }
 
 #[cfg(test)]
