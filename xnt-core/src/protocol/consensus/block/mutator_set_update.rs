89ed31cfbfe3f988fd7dfde78f1a0476d51da617
sword-smith <thor@neptune.cash>
2026-07-31 12:53:50 +0200

    fix(mutator_set): Off-by-one error in activity split
    Fix off-by-one error in activity split that could lead to a panic in a
    subsequent call to `ActiveWindow::contains`. This panic was only
    reachable through the receival of a PeerMessage::Transaction variant
    over the p2p network, and there, panics are guarded by catch-unwind.
    
    This closes (low severity) NPT-2 which could have caused a panic in a
    peer loop since transaction validation is not longer guarded by the
    proof validity check, after a reordering of the proof validity checks.
    
    Also addresses the same problem reachable through the JSON/RPC endpoint
    for transaction receival.

--- xnt-core/src/protocol/consensus/block/mutator_set_update.rs
+++ xnt-core/src/protocol/consensus/block/mutator_set_update.rs
@@ -152,3 +152,61 @@ impl MutatorSetUpdate {
         removal_records_are_valid
     }
 }
+
+#[cfg(test)]
+#[cfg_attr(coverage_nightly, coverage(off))]
+mod tests {
+    use proptest::collection::vec;
+    use proptest::prelude::TestCaseError;
+    use proptest::prop_assert;
+    use proptest_arbitrary_interop::arb;
+    use tasm_lib::prelude::Digest;
+    use test_strategy::proptest;
+
+    use super::MutatorSetUpdate;
+    use crate::util_types::mutator_set::msa_and_records::MsaAndRecords;
+
+    fn prop(msa_and_records: MsaAndRecords) -> std::result::Result<(), TestCaseError> {
+        let removal_records = msa_and_records.unpacked_removal_records();
+        for rr in &removal_records {
+            prop_assert!(msa_and_records.mutator_set_accumulator.can_remove(rr));
+        }
+
+        let original_msa = msa_and_records.mutator_set_accumulator;
+        for rr in removal_records {
+            let mut mutated_msa = original_msa.clone();
+            let as_msu = MutatorSetUpdate::new(vec![rr.clone()], vec![]);
+            prop_assert!(as_msu.apply_to_accumulator(&mut mutated_msa).is_ok());
+            prop_assert!(
+                !mutated_msa.can_remove(&rr),
+                "Can remove must return false after RR has been applied"
+            );
+        }
+
+        Ok(())
+    }
+
+    #[proptest]
+    fn can_remove_agrees_with_update_result_u8(
+        #[strategy(0usize..40)] _num_removals: usize,
+        #[strategy((#_num_removals as u64)..=(u64::from(u8::MAX)))] _num_leafs_aocl: u64,
+        #[strategy(vec((arb::<Digest>(), arb::<Digest>(), arb::<Digest>()), #_num_removals))]
+        _removables: Vec<(Digest, Digest, Digest)>,
+        #[strategy(MsaAndRecords::arbitrary_with((#_removables, #_num_leafs_aocl)))]
+        msa_and_records: MsaAndRecords,
+    ) {
+        prop_assert!(prop(msa_and_records).is_ok())
+    }
+
+    #[proptest(cases = 10)]
+    fn can_remove_agrees_with_update_result_u16(
+        #[strategy(0usize..40)] _num_removals: usize,
+        #[strategy((#_num_removals as u64)..=(u64::from(u16::MAX)))] _num_leafs_aocl: u64,
+        #[strategy(vec((arb::<Digest>(), arb::<Digest>(), arb::<Digest>()), #_num_removals))]
+        _removables: Vec<(Digest, Digest, Digest)>,
+        #[strategy(MsaAndRecords::arbitrary_with((#_removables, #_num_leafs_aocl)))]
+        msa_and_records: MsaAndRecords,
+    ) {
+        prop_assert!(prop(msa_and_records).is_ok())
+    }
+}
